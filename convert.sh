e0c5e2866d7ca4eb9ba293f6e446fa60e9b9313a
Laurent Pugin <lxpugin@gmail.com>
2023-05-24 10:20:42 +0200

    Add comments to the convert.sh script

--- convert.sh
+++ convert.sh
@@ -4,10 +4,15 @@ mscore="$MSCORE"
 test=$1
 
 cd $test
+
+# convert MSCZ to PDF and to MusicXML
 "$mscore" $test.mscz -o $test.pdf
 "$mscore" $test.mscz -o $test.musicxml
+
+# convert MusicXML to MEI Basic with Verovio
 verovio $test.musicxml -t mei-basic -o $test.basic.mei 
 
+# unzip the MSCZ to XML
 unzip -p $test.mscz $test.mscx > $test.ms.xml
 
 cd ..
