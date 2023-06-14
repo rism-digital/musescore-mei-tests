b0b615ecf2d6e08033e0635fe551dd6fa37a3b4b
Laurent Pugin <lxpugin@gmail.com>
2023-06-14 21:34:58 +0200

    Move .musicxml ms.xml and .basic.mei and add ms generated mei

--- convert.sh
+++ convert.sh
@@ -3,16 +3,19 @@ mscore="$MSCORE"
 # directory with corresponding test file
 test=$1
 
+
+
 cd $test
+mkdir reference
 
 # convert MSCZ to PDF and to MusicXML
 "$mscore" $test.mscz -o $test.pdf
-"$mscore" $test.mscz -o $test.musicxml
+"$mscore" $test.mscz -o reference/$test.musicxml
 
 # convert MusicXML to MEI Basic with Verovio
-verovio $test.musicxml -t mei-basic -o $test.basic.mei 
+verovio reference/$test.musicxml -t mei-basic -o reference/$test.basic.mei 
 
 # unzip the MSCZ to XML
-unzip -p $test.mscz $test.mscx > $test.ms.xml
+unzip -p $test.mscz $test.mscx > reference/$test.ms.xml
 
 cd ..
